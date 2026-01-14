ba7f73279afe861355cfa05286b593c74c7f59ca
Austin Strain <aust4035@colorado.edu>
2026-01-14 09:04:44 -0700

    Refactor comments for clarity in h_gps.m

--- src/models/h_gps.m
+++ src/models/h_gps.m
@@ -3,14 +3,14 @@ function y = h_gps(x)
 %   State: x = [px; py; vx; vy]
 %   Measurement: y = [px; py]
 
-% TODO: assert x is 4x1
+% Assert x is 4x1
 assert(isequal(size(x), [4 1]), 'x must be 4x1');
 
-% TODO: extract px and py
+% Extract px and py
 px = x(1);
 py = x(2);
 
-% TODO: pack y as 2x1
+% Pack y as 2x1
 y = [px; py];
 
 end
